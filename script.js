8230df32af85997376459d0f762fa50ee9ebc2ce
monikahenschke <monika.henschke@gmail.com>
2021-02-28 01:17:08 +0100

    change the information about currently plaing radio station

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 const stations = document.querySelectorAll('.station');
+const currentRadioStationName = document.querySelector('.station-name');
+
 let currentRadioStation;
 
 function stationSelection(){
@@ -8,10 +10,14 @@ function stationSelection(){
     if(currentRadioStation) {
         currentRadioStation.pause();
     }
-    let audio = this.querySelector('.audio');
-    audio.play();
-    currentRadioStation = audio;
 
+    let audio = this.querySelector('.audio');
+    let name = this.querySelector('span');
+
+    currentRadioStationName.innerHTML = name.innerText;
+    audio.play();
+    
+    currentRadioStation = audio;
 }
 
 stations.forEach(station => station.addEventListener('dblclick', stationSelection));
