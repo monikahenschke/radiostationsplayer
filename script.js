d86d820036c529b0a88f1fcad8e277f21d935002
monikahenschke <monika.henschke@gmail.com>
2021-02-28 15:03:37 +0100

    general appearance of the player 2

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const radioStations = [
+    {name: 'cat-country',link:'http://17573.live.streamtheworld.com/WCTKFMAAC.aac'},
+    {name: 'americas-country', link:'https://ais-sa2.cdnstream1.com/1976_128.mp3'},
+    {name: 'ktwb-big-country', link:'https://14083.live.streamtheworld.com/KTWBFMAAC.aac'},
+    {name: 'country-208', link:'http://icepool.silvacast.com/COUNTRY108.mp3'},
+    {name: 'kickin-country', link:'https://listen.181fm.com/181-kickincountry_128k.mp3'}
+];
