0c4e62fa93fd47b9314ef33f6977558e2f16c4b0
monikahenschke <monika.henschke@gmail.com>
2021-03-01 16:17:17 +0100

    optimization to one audio tag

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 
-<<<<<<< HEAD
 
-const stations = document.querySelectorAll('.station');
-let currentRadioStation;
+window.addEventListener('DOMContentLoaded',() => {
 
-function stationSelection(){
+    const radioStations = [
+        {name: 'cat-country',link:'http://17573.live.streamtheworld.com/WCTKFMAAC.aac'},
+        {name: 'americas-country', link:'https://ais-sa2.cdnstream1.com/1976_128.mp3'},
+        {name: 'ktwb-big-country', link:'https://14083.live.streamtheworld.com/KTWBFMAAC.aac'},
+        {name: 'country-208', link:'http://icepool.silvacast.com/COUNTRY108.mp3'},
+        {name: 'kickin-country', link:'https://listen.181fm.com/181-kickincountry_128k.mp3'}
+    ];
+    
+    const stations = document.querySelectorAll('.station');
+    const audio = document.querySelector('#audio');
+    let currentRadioStation;
+    
+    function stationSelection(){
 
-    if(currentRadioStation) {
-        currentRadioStation.pause();
+        currentRadioStation = this.getAttribute('data-station-id');
+
+        radioStations.forEach(function(radioStation) {
+
+            if(radioStation.name === currentRadioStation) {
+                audio.src = radioStation.link;
+            }
+        });
+
+        audio.play();
+    
     }
-    let audio = this.querySelector('.audio');
-    audio.play();
-    currentRadioStation = audio;
+    
+    stations.forEach(station => station.addEventListener('dblclick', stationSelection));
 
-}
 
-stations.forEach(station => station.addEventListener('dblclick', stationSelection));
-
-=======
-const radioStations = [
-    {name: 'cat-country',link:'http://17573.live.streamtheworld.com/WCTKFMAAC.aac'},
-    {name: 'americas-country', link:'https://ais-sa2.cdnstream1.com/1976_128.mp3'},
-    {name: 'ktwb-big-country', link:'https://14083.live.streamtheworld.com/KTWBFMAAC.aac'},
-    {name: 'country-208', link:'http://icepool.silvacast.com/COUNTRY108.mp3'},
-    {name: 'kickin-country', link:'https://listen.181fm.com/181-kickincountry_128k.mp3'}
-];
->>>>>>> appearance
+});
